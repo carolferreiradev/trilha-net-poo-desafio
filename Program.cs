67f806c5aa7d801305a920cfe62716ab9a1f0c65
Carol Ferreira <developer.anacarolina@gmail.com>
2024-11-13 15:33:50 -0300

    test: Add tests to class Nokia and Iphone

--- Program.cs
+++ Program.cs
@@ -1,3 +1,11 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Iphone iphone = new Iphone("16", "Plus", "956303480948781", 256);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Whatsapp");
+
+Nokia nokia = new Nokia("8", "Sirocco", "999303480948781", 128);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Facebook");
